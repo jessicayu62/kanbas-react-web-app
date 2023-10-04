697b30dd530abc97457b3a29a366ca891d4e79b9
Jessica Yu <jessicayu62@gmail.com>
2023-10-04 15:07:03 -0400

    lab

---
+++ src/Labs/a3/JavaScript.js
@@ -0,0 +1,22 @@
+import BooleanVariable from "./BooleanVariables";
+import VariableTypes from "./VariableTypes";
+import VariablesAndConstants from "./VariablesAndConstants";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import WorkingWithFunctions from "./WorkingWithFunctions"
+
+function JavaScript() {
+   console.log('Hello World!');
+   return (
+      <div>
+         <h1>JavaScript</h1>
+         <VariablesAndConstants />
+         <VariableTypes />
+         <BooleanVariable />
+         <IfElse />
+         <TernaryOperator />
+         <WorkingWithFunctions />
+      </div>
+   );
+}
+export default JavaScript
